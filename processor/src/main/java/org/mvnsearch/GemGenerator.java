97eb92d06d6a1c5321f829b9c97b0738a0b4db72
linux_china <libing.chen@gmail.com>
2020-05-08 08:11:12 -0700

    GemGenerator for annotations

---
+++ processor/src/main/java/org/mvnsearch/GemGenerator.java
@@ -0,0 +1,8 @@
+package org.mvnsearch;
+
+import org.mapstruct.tools.gem.GemDefinition;
+import org.mapstruct.tools.gem.GemDefinitions;
+
+@GemDefinitions(value = @GemDefinition(value = Mapping.class))
+public interface GemGenerator {
+}
