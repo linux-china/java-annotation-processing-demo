551345e8646797ee78d69e36ebc73b556068b3fe
linux_china <libing.chen@gmail.com>
2020-05-08 08:11:27 -0700

    Mapping annotation processor

---
+++ processor/src/main/java/org/mvnsearch/MappingProcessor.java
@@ -0,0 +1,67 @@
+package org.mvnsearch;
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Mapping annotation processor
+ *
+ * @author linux_china
+ */
+@AutoService(Processor.class)
+public class MappingProcessor extends AbstractProcessor {
+
+    boolean processed;
+    private Filer filer;
+    private Messager messager;
+
+    @Override
+    public Set<String> getSupportedOptions() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton("org.mvnsearch.Mapping");
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        if (SourceVersion.latest().compareTo(SourceVersion.RELEASE_8) > 0) {
+            return SourceVersion.latest();
+        } else {
+            return SourceVersion.RELEASE_8;
+        }
+    }
+
+    @Override
+    public void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        this.filer = processingEnv.getFiler();
+        this.messager = processingEnv.getMessager();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (Element element : roundEnv.getElementsAnnotatedWith(Mapping.class)) {
+            MappingGem mappingGem = MappingGem.instanceOn(element);
+            System.out.println(mappingGem);
+            //processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "it should be final");
+        }
+        this.processed = true;
+        return false;
+    }
+
+    @Override
+    public Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation, ExecutableElement member, String userText) {
+        return Collections.emptyList();
+    }
+}
